094aa190daa9508a0467072ffc1884b815de2cea
ThomasBertiere <ThomasBertiere@10.32.2.220>
2016-03-30 13:11:18 +0200

    ajout classe chemin

---
+++ BE/src/core/Chemin.java
@@ -0,0 +1,20 @@
+package core;
+
+import java.util.Deque;
+
+public class Chemin {
+	
+	private Sommets PremierSommet;
+	private Sommets DernierSommet;
+	private int NbSommets;
+	private Deque<Sommets> FileSommets; //c'est une file, on accède au 1er ou au dernier
+	
+	public Chemin(Sommets PremierSommet, Sommets DernierSommet, int NbSommets, ArrayList<Sommets> ListeSommets, String NomCarte){
+		this.PremierSommet=PremierSommet;
+		this.DernierSommet=DernierSommet;
+		this.NbSommets=NbSommets;
+		
+	}
+	
+
+}
