8682f4ddf84ad95ff71098f642151741dd08e4d2
ThomasBertiere <ThomasBertiere@10.32.2.220>
2016-03-30 14:13:34 +0200

    commit chemin

--- BE/src/core/Chemin.java
+++ BE/src/core/Chemin.java
@@ -1,21 +1,29 @@
 package core;
 
-import java.util.Deque;
+import java.util.ArrayList;
 
 public class Chemin {
 	
 	private int nbSommets;
 	private int idCarte;
-	private Deque<Sommets> fileSommets; //c'est une file, on accède au 1er ou au dernier
+	private ArrayList<Sommets> listeSommets; //c'est une file, on accède au 1er ou au dernier
 	
-	public Chemin(int nbSommets, Sommets[] tabSommets, String nomCarte, int idCarte){
+	public Chemin(int nbSommets, ArrayList<Sommets> listeSommets, int idCarte){
 		this.idCarte=idCarte;
-		for (int i=0;i<nbSommets;i++){
-			this.fileSommets.add(tabSommets[i]);
-		}		
+		this.listeSommets=listeSommets;
 	}
+		
 	
+	public float calculTemps(){
 	
+		int tailleListe=this.listeSommets.size();
+		
+		for(int i=0;i<tailleListe;i++){
+			//recherche de l'arrète entre le sommet i et i+1
+			
+		}
+	
+	}
 	
 
 }
